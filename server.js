d1bc7568caa86dd60014f90b99f9db0c82b056e0
YvesJaques <ymoraisjaques@gmail.com>
2024-03-09 20:29:03 -0300

    Implemented ShortUrl creation

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
 const app =  express();
 
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/urlShortener')
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false })); // tell express we are working with urls as parameters
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls });
+})
+
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
+    res.redirect('/')
 })
 
 app.listen(process.env.PORT || 5000)
