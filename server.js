9dfa533a3c3af925de3c572b11365ad9b6fd567c
YvesJaques <ymoraisjaques@gmail.com>
2024-03-09 20:24:16 -0300

    Added dotenv & mongodb connection

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app =  express();
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/urlShortener')
 
 app.set('view engine', 'ejs');
 
